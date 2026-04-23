867bd34492c13998be7937f9987a865961e313f7
Djazil4ik <mustafaevdjaz@gmail.com>
2026-04-23 19:49:31 +0500

    feat(translation): register categories, types and additional product fields for modeltranslation

--- products/translation.py
+++ products/translation.py
@@ -1,15 +1,31 @@
 from modeltranslation.translator import register, TranslationOptions
-from .models import Products, ProductsAdvantage, ProductsFAQ
+from .models import Products, ProductsAdvantage, ProductsFAQ, Type, Second_Category, Categories
 
 
 @register(Products)
 class ProductTranslationOptions(TranslationOptions):
-    fields = ('sub_title', 'advantages',)
+    fields = ('sub_title', 'advantages', 'warranty', 'payment',)
+
 
 @register(ProductsAdvantage)
 class ProductsAdvantageTranslationOptions(TranslationOptions):
     fields = ('advantage',)
 
+
 @register(ProductsFAQ)
 class ProductsFAQTranslationOptions(TranslationOptions):
     fields = ('faq', 'answer',)
+
+
+@register(Type)
+class TypeTranslationOptions(TranslationOptions):
+    fields = ('product_type',)
+
+
+@register(Second_Category)
+class SecondCategoryTranslationOptions(TranslationOptions):
+    fields = ('second_category',)
+
+@register(Categories)
+class CategoriesTranslationOptions(TranslationOptions):
+    fields = ('category',)
