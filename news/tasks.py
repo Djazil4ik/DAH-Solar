072f18df6371c7e50f005139c71f8d17583c6d5f
Djazil4ik <mustafaevdjaz@gmail.com>
2026-06-25 22:07:38 +0500

    feat: add translation check for news/tasks.py

--- news/tasks.py
+++ news/tasks.py
@@ -6,17 +6,17 @@ from celery import shared_task
 @shared_task
 def translate_news_category_task(category_id):
     category = NewsCategory.objects.get(id=category_id)
-    if not category:
-        return
 
     update_fields = {}
 
     if category.category_name:
-        update_fields['category_name_ru'] = safe_translate(
-            text=category.category_name, src='en', dest='ru')
-        update_fields['category_name_uz'] = safe_translate(
-            text=category.category_name, src='en', dest='uz')
-        
+        if not category.category_name_ru: #type: ignore
+            update_fields['category_name_ru'] = safe_translate(
+                text=category.category_name, src='en', dest='ru')
+        if not category.category_name_uz: #type: ignore
+            update_fields['category_name_uz'] = safe_translate(
+                text=category.category_name, src='en', dest='uz')
+
     if update_fields:
         NewsCategory.objects.filter(id=category_id).update(**update_fields)
 
@@ -24,17 +24,17 @@ def translate_news_category_task(category_id):
 @shared_task
 def translate_news_task(news_id):
     news = News.objects.get(id=news_id)
-    if not news:
-        return
 
     update_fields = {}
 
     if news.news_title:
-        update_fields['news_title_ru'] = safe_translate(
-            text=news.news_title, src='en', dest='ru')
-        update_fields['news_title_uz'] = safe_translate(
-            text=news.news_title, src='en', dest='uz')
-        
+        if not news.news_title_ru: #type: ignore
+            update_fields['news_title_ru'] = safe_translate(
+                text=news.news_title, src='en', dest='ru')
+        if not news.news_title_uz: #type: ignore
+            update_fields['news_title_uz'] = safe_translate(
+                text=news.news_title, src='en', dest='uz')
+
     if update_fields:
         News.objects.filter(id=news_id).update(**update_fields)
 
@@ -42,8 +42,17 @@ def translate_news_task(news_id):
 @shared_task
 def translate_news_image_task(image_id):
     image_obj = NewsImage.objects.filter(id=image_id).first()
+    if not image_obj or not image_obj.body_text:
+        return
 
-    NewsImage.objects.filter(id=image_id).update(
-        body_text_ru=translate_html(image_obj.body_text, src='en', dest='ru'), # type: ignore
-        body_text_uz=translate_html(image_obj.body_text, src='en', dest='uz') # type: ignore
-    )
+    update_fields = {}
+
+    if not image_obj.body_text_ru: #type: ignore
+        update_fields['body_text_ru'] = translate_html(
+            image_obj.body_text, src='en', dest='ru')
+    if not image_obj.body_text_uz: #type: ignore
+        update_fields['body_text_uz'] = translate_html(
+            image_obj.body_text, src='en', dest='uz')
+
+    if update_fields:
+        NewsImage.objects.filter(id=image_id).update(**update_fields)
